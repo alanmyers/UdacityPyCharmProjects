4334c6e892f88c7df1cdb09e17b5be6cc229db4d
alanmyers <alan.myers@verizon.com>
2018-04-30 07:13:15 -0600

    Initial Versions

---
+++ Udacity/OOO/child.py
@@ -0,0 +1,8 @@
+import parent
+
+
+class Child(parent.Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor: ", last_name, " ", eye_color, number_of_toys)
+        parent.Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
---
+++ Udacity/OOO/inheritance.py
@@ -0,0 +1,5 @@
+import parent, child
+
+miley_cyrus = child.Child("Cyrus", "Blue", 5)
+print(miley_cyrus.last_name)
+print(miley_cyrus.number_of_toys)
